d6e16f1ae97e00ec517f9fe72c70803914bc0d91
notadeejay <amyemansell@gmail.com>
2021-02-13 11:20:46 -0500

    styled buttons

--- client/src/components/ProductModal.js
+++ client/src/components/ProductModal.js
@@ -20,6 +20,17 @@ handleClick = (e) => {
 }
   render() {
   
+  const renderButtons = (product) => {
+    let timeOfDay = product.am_pm
+    if (timeOfDay.includes("AM") && timeOfDay.includes("PM")) {
+      return <React.Fragment><button onClick={this.handleClick} routine-id="AM" product-id={product.id} className="add__morning">Add to AM Routine</button> 
+      <button onClick={this.handleClick} routine-id="PM" product-id={product.id} className="add__evening">Add to PM Routine</button></React.Fragment>
+    } else if (timeOfDay.includes("AM")) {
+      return <button onClick={this.handleClick} routine-id="AM" product-id={product.id} className="add__morning">Add to AM Routine</button>
+    } else {
+      return <button onClick={this.handleClick} routine-id="PM" product-id={product.id} className="add__evening">Add to PM Routine</button>
+    }
+  }
 
     return (
       <div className="product-modal__backdrop" onClick={this.props.close}>
@@ -29,9 +40,21 @@ handleClick = (e) => {
             return <div  className="product-details">
               <h2>{product.name}</h2>
               <p>{product.description}</p>
+              <span className="product__category">{product.category}</span>
+              <div className="additional-details">
+            <div className="targets">
+               <p>Targets</p>
+                <ul className="product__targets">{product.targets.map((target) => {
+                  return <li>{target}</li>})}
+                </ul>
+            </div>
+            <div>
+              <p>When to Use</p>
+              <ul className="product__use"><li>{product.when_to_use}</li></ul>
+            </div>
+          </div>
               <div className="routine-controls">
-                <button onClick={this.handleClick} routine-id="AM" product-id={product.id} className="add__morning">Add to AM Routine</button>
-                <button onClick={this.handleClick} routine-id="PM" product-id={product.id} className="add__evening">Add to PM Routine</button>
+                {renderButtons(product)}
               </div>
             </div>
            }
